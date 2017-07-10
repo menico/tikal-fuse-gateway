a66f140eeaae4630bcbf9504956ccb27c6c35b48
Gal Noy <gal@tikalk.com>
2017-07-10 16:30:38 +0300

    updates to passport

--- src/index.js
+++ src/index.js
@@ -1,20 +1,48 @@
 const express = require('express')
-const httpProxy = require('http-proxy')
+const proxy = require('http-proxy')
+const session = require('express-session')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const partials = require('express-partials')
+const path = require('path')
 
 const getServiceHost = name => name // get from discovery
 
 const app = express()
-const passport = require('passport')
+const passport = require('./passport')
+console.log(path.join(__dirname, '../views'))
+
+app.use(partials())
+app.use(bodyParser.json())
+app.use(methodOverride())
+app.use(session({secret: 'keyboard cat', cookie: { maxAge: 60000 }}))
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize())
+app.use(passport.session())
+// app.use(express.static(__dirname + '/public'))
+
+app.get('/', function (req, res) {
+  res.send("I'm alive")
+})
+
+// app.get('/login', function (req, res) {
+//   res.render('login', { user: req.user })
+// })
 
 app.get('/auth/github',
   passport.authenticate('github', { scope: [ 'user:email' ] }))
 
-app.get('/auth/github/callback', 
+app.get('/auth/github/callback',
   passport.authenticate('github', { failureRedirect: '/login' }),
-  function(req, res) {
+  function (req, res) {
     // Successful authentication, redirect home.
-    res.redirect('/');
+    res.redirect('/')
   })
+
+app.use('/api', (req, res, next) =>
+  req.isAuthenticated() ? next() : res.redirect('/auth/github')
+)
 
 app.use('/api/author', (req, res, next) =>
   proxy.web(req, res, {target: getServiceHost('author')}))
@@ -27,3 +55,7 @@ app.use('/api/roadmap', (req, res, next) =>
 
 app.use('/api/student', (req, res, next) =>
   proxy.web(req, res, {target: getServiceHost('student')}))
+
+app.use((err, req, res, next) => res.status(400).send(err))
+
+app.listen(3000)
