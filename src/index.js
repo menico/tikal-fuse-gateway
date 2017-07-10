fccdb980aaea75999e363681a7802e5181f9b80f
Gal Noy <gal@tikalk.com>
2017-07-10 17:45:57 +0300

    connected to user-service

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const getServiceHost = name => name // get from discovery
 
 const app = express()
 const passport = require('./passport')
-console.log(path.join(__dirname, '../views'))
+// console.log(path.join(__dirname, '../views'))
 
 app.use(partials())
 app.use(bodyParser.json())
@@ -23,7 +23,7 @@ app.use(passport.session())
 // app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
-  res.send("I'm alive")
+  res.send("I'm alive " + (req.isAuthenticated() ? 'Authenticated' : 'Not Authenticated'))
 })
 
 // app.get('/login', function (req, res) {
@@ -34,7 +34,7 @@ app.get('/auth/github',
   passport.authenticate('github', { scope: [ 'user:email' ] }))
 
 app.get('/auth/github/callback',
-  passport.authenticate('github', { failureRedirect: '/login' }),
+  passport.authenticate('github', { failureRedirect: '/auth/github' }),
   function (req, res) {
     // Successful authentication, redirect home.
     res.redirect('/')
--- src/passport.js
+++ src/passport.js
@@ -1,16 +1,26 @@
 const passport = require('passport')
 const GitHubStrategy = require('passport-github2').Strategy
+require('isomorphic-fetch')
+
+const getService = name => name
 
 passport.use(new GitHubStrategy({
   clientID: 'f56b753f4fb79c86df7d',
   clientSecret: '05d0f1578a574d2f283a5234079703b4662ae719',
-  callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+  callbackURL: 'http://localhost:3000/auth/github/callback'
 },
 function (accessToken, refreshToken, profile, done) {
-  // User.findOrCreate({ githubId: profile.id }, function (err, user) {
-  //   return done(err, user);
-  // });
-  // call user service save/create and save the token
+  fetch(getService('user-service') + '/user/register', {method: 'POST', body: profile})
+    .then((response) => {
+      if (response.status >= 400) {
+        throw new Error('Bad response from server')
+      }
+      return response.json()
+    })
+    .then(res => {
+      res.set('Authorization', 'Bearer ' + res.jwt)
+      done()
+    })
 }
 ))
 
